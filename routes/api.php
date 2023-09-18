c997b593a194757144a8744ddce5a7c4de8fbd61
César Mora <cesar.mcid@gmail.com>
2023-09-18 20:57:42 -0300

    [refactor] Actualizar ruta

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,4 @@
 use Illuminate\Support\Facades\Route;
 use xcesaralejandro\lti1p3\Http\Controllers\Lti1p3Controller;
 
-Route::prefix('api/lti1p3')->group(function () {
-    Route::get('/jwks', [Lti1p3Controller::class, 'jwks'])->middleware('api');
-});
+Route::get('api/lti1p3/jwks', [Lti1p3Controller::class, 'jwks'])->middleware('api');
